97540dbc15651c9ebd5b3a3593149ac587e65d85
Gary Greene <greeneg@tolharadys.net>
2017-08-19 17:06:03 -0400

    initial commit
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

---
+++ README.md
@@ -0,0 +1,6 @@
+# Discovery
+
+A tool that discovers and reports useful information about a system. When it
+is run, it gathers up "truths" as heirarchical key/value pairs and reports
+them as JSON, simple key/value data, or as a Perl Hash structure for use by
+many tools.
---
+++ lib/Discovery/Constants.pm
@@ -0,0 +1,65 @@
+#!/usr/bin/env perl -T
+#
+# Author: Gary Greene <greeneg@tolharadys.net>
+# Copyright: 2017 YggdrasilSoft, LLC. All Rights Reserved
+#
+##########################################################################
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#    
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. 
+#
+
+package Discovery::Constants;
+
+use strict;
+use warnings;
+use English;
+use utf8;
+
+BEGIN {
+    use Exporter   ();
+
+    our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+    # set the version for version checking
+    $VERSION     = 1.0;
+    @ISA         = qw(Exporter);
+    @EXPORT      = qw();
+    %EXPORT_TAGS = ( All => [ qw($version $author $license $login_title $title $copyright) ] );     # eg: TAG => [ qw!name1 name2! ],
+
+    # your exported package globals go here,
+    # as well as any optionally exported functions
+    @EXPORT_OK   = qw($version $author $license $login_title $title $copyright);
+}
+
+our @EXPORT_OK;
+
+# exported package globals go here
+our $author;
+our $version;
+our $copyright;
+our $license;
+our $login_title;
+our $title;
+
+# initialize package globals, first exported ones
+$version     = 0.5;
+$author      = 'Gary L. Greene, Jr.';
+$copyright   = 'Copyright 2017 YggdrasilSoft, LLC. All Rights Reserved.';
+$license     = 'Licensed under the Apache Public License version 2.0';
+$title       = 'ConfMan Discovery';
+$login_title = "$title - login";
+
+END { }       # module clean-up code here (global destructor)
+
+1;  # don't forget to return a true value from the file
+                
