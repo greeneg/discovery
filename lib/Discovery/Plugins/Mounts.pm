8e244bca08545fbd4edc2b0c9104ae6d2c2fbf7b
Gary Greene <greeneg@tolharadys.net>
2019-11-22 17:39:32 -0500

    Mounts plugin, Linux implementation
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

---
+++ lib/Discovery/Plugins/Mounts.pm
@@ -0,0 +1,96 @@
+#!/usr/bin/env perl
+
+#########################################################################################
+#                                                                                       #
+# Application: discovery.pl                                                             #
+# Summary:     A System Discovery Tool                                                  #
+# Author:      Gary L. Greene, Jr. <greeneg@yggdrasilsoft.com>                          #
+# Copyright:   2011-2019 YggdrasilSoft, LLC.                                            #
+# License:     Apache Public License, v2                                                #
+#                                                                                       #
+#=======================================================================================#
+#                                                                                       #
+# Licensed under the Apache License, Version 2.0 (the "License");                       #
+# you may not use this file except in compliance with the License.                      #
+# You may obtain a copy of the License at                                               #
+#                                                                                       #
+#     http://www.apache.org/licenses/LICENSE-2.0                                        #
+#                                                                                       #
+# Unless required by applicable law or agreed to in writing, software                   #
+# distributed under the License is distributed on an "AS IS" BASIS,                     #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.              #
+# See the License for the specific language governing permissions and                   #
+# limitations under the License.                                                        #
+#                                                                                       #
+#########################################################################################
+
+package Discovery::Plugins::Mounts;
+
+use strict;
+use warnings;
+use feature ":5.22";
+# Add features to system for lexical subs and signatures
+# disable all warnings for these as they are still experimental
+# (likely won't change much though in the future...)
+no warnings "experimental::lexical_subs";
+no warnings "experimental::signatures";
+use feature 'lexical_subs';
+use feature 'signatures';
+use English;
+use utf8;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use boolean;
+use File::Slurp;
+use Data::Dumper;
+
+sub new ($class) {
+    my $self = {};
+    bless $self, $class;
+
+    return $self;   
+}
+
+my sub get_mounts ($self, $os, $debug) {
+    my %mounts;
+    if ($os eq 'linux') {
+        my @mtab = read_file('/proc/mounts');
+        foreach my $mnt_record (@mtab) {
+            $mnt_record =~ s/\n//;
+            say STDERR "MTAB RECORD: $mnt_record";
+            my ($source, $target, $fs, $mnt_opts, $dump, $fsck) = split(' ', $mnt_record);
+            say STDERR "SOURCE: $source";
+            if ($source =~ m/^\/dev\/.*$/) {
+                $mounts{'Disks'}->{$source} = {
+                    'target' => $target,
+                    'filesystem' => $fs,
+                    'options' => $mnt_opts,
+                    'dump' => $dump,
+                    'fs_check' => $fsck
+                };
+            } else {
+                $mounts{'VirtualFilesysmts'}->{$source} = {
+                    'target' => $target,
+                    'filesystem' => $fs,
+                    'options' => $mnt_opts,
+                    'dump' => $dump,
+                    'fs_check' => $fsck
+                };
+            }
+        }
+    }
+
+    return \%mounts;
+}
+
+our sub runme ($self, $os, $debug) {
+    my %values;
+
+    $values{'Mounts'} = get_mounts($self, $os, $debug);
+
+    return %values;
+}
+
+true;
